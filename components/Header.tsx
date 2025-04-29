73a6a0d89b323b55f868e4bf94d2319002857b4d
mixajil.NODESautomation <mixajil@gmail.com>
2025-04-29 15:38:21 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,8 @@ export default function Header() {
     <header className="bg-emerald-500/90 dark:bg-emerald-700/90 text-white p-4 backdrop-blur-sm">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="flex items-center space-x-2">
-          <Image
-            src="https://s3.amazonaws.com/keybase_processed_uploads/a1239fd297760e3b35df576a5ca6f005_360_360.jpg"
-            alt="NODESautomation Logo"
-            width={40}
-            height={40}
-            className="rounded-full"
-          />
-          <span className="text-xl font-bold text-shadow">NODESautomation</span>
+       <Image src="/img/NA_logo.png" alt="NODESautomation Logo" width={40} height={40} className="rounded-full" />
+        <span className="text-xl font-bold text-shadow">NODESautomation</span>
         </Link>
         <div className="flex items-center space-x-4">
           <button
